b2372ce513b3cc96d9ec18d11de14345d58bf516
W7VI <1057664619@qq.com>
2022-12-11 11:19:49 +0800

    leetcode 74

--- src/main/test/local/Solution.java
+++ src/main/test/local/Solution.java
@@ -60,8 +60,43 @@ public class Solution {
 
 //        System.out.println(s.nthSuperUglyNumber(12, new int[]{2, 7, 13, 19}));
 //        s.topKFrequent(new String[]{"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"}, 2);
-        System.out.println(s.numDecodings("12"));
-//        System.out.println(s.numDecodings("2611"));
+//        System.out.println(s.numDecodings("12"));
+        System.out.println(s.searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 16));
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = binarySearchRow(matrix, target);
+//        for (; row < matrix.length - 1; row++) {
+//            if (matrix[row + 1][0] > target) {
+//                break;
+//            }
+//        }
+
+        int col = 0;
+        for (; col < matrix[row].length; col++) {
+            if (matrix[row][col] == target) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int binarySearchRow(int[][] matrix, int target) {
+        int head = 0, tail = matrix.length - 1;
+        int mid;
+        while (head < tail) {
+            mid = head + (tail - head) / 2;
+            if (matrix[mid][0] == target) {
+                return mid;
+            }
+            if (matrix[mid][0] < target) {
+                head = mid + 1;
+            } else {
+                tail = mid - 1;
+            }
+        }
+        return head;
     }
 
     public int numDecodings(String s) {
