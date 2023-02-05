4dbb2b2b402692e29ba1d1cf6bcac1ef30143903
wqw <1057664619@qq.com>
2023-02-05 15:53:43 +0800

    add shutdown api

--- src/main/java/springboot/demo/util/CommandUtil.java
+++ src/main/java/springboot/demo/util/CommandUtil.java
@@ -1,11 +1,14 @@
 package springboot.demo.util;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class CommandUtil {
 
     public static String run(String command) throws IOException {
+        Scanner input = null;
         String result = "";
         Process process = null;
         try {
@@ -16,6 +19,12 @@ public class CommandUtil {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            InputStream is = process.getInputStream();
+            input = new Scanner(is);
+            while (input.hasNextLine()) {
+                result += input.nextLine() + "\n";
+            }
+            result = command + "\n" + result;
         } finally {
             if (process != null) {
                 process.destroy();
