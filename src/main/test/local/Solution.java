aa89cf941c3af9da8c95d083e02ab7013fd5501a
wqw <1057664619@qq.com>
2023-02-26 21:53:42 +0800

    leetcode 95

--- src/main/test/local/Solution.java
+++ src/main/test/local/Solution.java
@@ -82,7 +82,9 @@ public class Solution {
 //        List ac = new ArrayList(Arrays.asList(a1, a2, a3, a4, a5));
 //        s.accountsMerge(ac);
 
-        System.out.println(s.lengthOfLastWord("   fly me   to   the moon  "));
+//        System.out.println(s.lengthOfLastWord("   fly me   to   the moon  "));
+        List<TreeNode> treeNodes = s.generateTrees(3);
+        System.out.println(treeNodes);
     }
 
     public List<Integer> majorityElement(int[] nums) {
@@ -118,50 +120,50 @@ public class Solution {
         return res;
     }
 
-    public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        Map<String, Integer> emailToIndex = new HashMap<String, Integer>();
-        Map<String, String> emailToName = new HashMap<String, String>();
-        int emailsCount = 0;
-        for (List<String> account : accounts) {
-            String name = account.get(0);
-            int size = account.size();
-            for (int i = 1; i < size; i++) {
-                String email = account.get(i);
-                if (!emailToIndex.containsKey(email)) {
-                    emailToIndex.put(email, emailsCount++);
-                    emailToName.put(email, name);
-                }
-            }
-        }
-        UnionFind uf = new UnionFind(emailsCount);
-        for (List<String> account : accounts) {
-            String firstEmail = account.get(1);
-            int firstIndex = emailToIndex.get(firstEmail);
-            int size = account.size();
-            for (int i = 2; i < size; i++) {
-                String nextEmail = account.get(i);
-                int nextIndex = emailToIndex.get(nextEmail);
-                uf.union(firstIndex, nextIndex);
-            }
-        }
-        Map<Integer, List<String>> indexToEmails = new HashMap<Integer, List<String>>();
-        for (String email : emailToIndex.keySet()) {
-            int index = uf.find(emailToIndex.get(email));
-            List<String> account = indexToEmails.getOrDefault(index, new ArrayList<String>());
-            account.add(email);
-            indexToEmails.put(index, account);
-        }
-        List<List<String>> merged = new ArrayList<List<String>>();
-        for (List<String> emails : indexToEmails.values()) {
-            Collections.sort(emails);
-            String name = emailToName.get(emails.get(0));
-            List<String> account = new ArrayList<String>();
-            account.add(name);
-            account.addAll(emails);
-            merged.add(account);
-        }
-        return merged;
-    }
+//    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+//        Map<String, Integer> emailToIndex = new HashMap<String, Integer>();
+//        Map<String, String> emailToName = new HashMap<String, String>();
+//        int emailsCount = 0;
+//        for (List<String> account : accounts) {
+//            String name = account.get(0);
+//            int size = account.size();
+//            for (int i = 1; i < size; i++) {
+//                String email = account.get(i);
+//                if (!emailToIndex.containsKey(email)) {
+//                    emailToIndex.put(email, emailsCount++);
+//                    emailToName.put(email, name);
+//                }
+//            }
+//        }
+//        UnionFind uf = new UnionFind(emailsCount);
+//        for (List<String> account : accounts) {
+//            String firstEmail = account.get(1);
+//            int firstIndex = emailToIndex.get(firstEmail);
+//            int size = account.size();
+//            for (int i = 2; i < size; i++) {
+//                String nextEmail = account.get(i);
+//                int nextIndex = emailToIndex.get(nextEmail);
+//                uf.union(firstIndex, nextIndex);
+//            }
+//        }
+//        Map<Integer, List<String>> indexToEmails = new HashMap<Integer, List<String>>();
+//        for (String email : emailToIndex.keySet()) {
+//            int index = uf.find(emailToIndex.get(email));
+//            List<String> account = indexToEmails.getOrDefault(index, new ArrayList<String>());
+//            account.add(email);
+//            indexToEmails.put(index, account);
+//        }
+//        List<List<String>> merged = new ArrayList<List<String>>();
+//        for (List<String> emails : indexToEmails.values()) {
+//            Collections.sort(emails);
+//            String name = emailToName.get(emails.get(0));
+//            List<String> account = new ArrayList<String>();
+//            account.add(name);
+//            account.addAll(emails);
+//            merged.add(account);
+//        }
+//        return merged;
+//    }
 
 
     public int[] findRedundantConnection(int[][] edges) {
@@ -1527,18 +1529,32 @@ public class Solution {
     }
 
     public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generateTrees(new int[]{1, n});
+    }
+
+    public List<TreeNode> generateTrees(int[] window) {
         List<TreeNode> res = new ArrayList<>();
-
-        List<TreeNode> allNodes = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            TreeNode node = new TreeNode(i);
-            allNodes.add(node);
+        if (window[0] > window[1]) {
+            res.add(null);
+            return res;
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = window[0]; i <= window[1]; i++) {
+            List<TreeNode> left = generateTrees(new int[]{window[0], i - 1});
+            List<TreeNode> right = generateTrees(new int[]{i + 1, window[1]});
 
+            for(TreeNode leftNode : left){
+                for(TreeNode rightNode : right){
+                    TreeNode root = new TreeNode(i);
+                    res.add(root);
+                    root.left = leftNode;
+                    root.right = rightNode;
+                }
+            }
         }
-
         return res;
     }
 
@@ -1616,24 +1632,24 @@ public class Solution {
 
 }
 
-class UnionFind {
-    int[] parent;
-
-    public UnionFind(int n) {
-        parent = new int[n];
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-    }
-
-    public void union(int index1, int index2) {
-        parent[find(index2)] = find(index1);
-    }
-
-    public int find(int index) {
-        if (parent[index] != index) {
-            parent[index] = find(parent[index]);
-        }
-        return parent[index];
-    }
-}
+//class UnionFind {
+//    int[] parent;
+//
+//    public UnionFind(int n) {
+//        parent = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            parent[i] = i;
+//        }
+//    }
+//
+//    public void union(int index1, int index2) {
+//        parent[find(index2)] = find(index1);
+//    }
+//
+//    public int find(int index) {
+//        if (parent[index] != index) {
+//            parent[index] = find(parent[index]);
+//        }
+//        return parent[index];
+//    }
+//}
